226453ba802abc4cd26879c60d35e1b03d4010c0
yedist <yedist10@gmail.com>
2025-11-12 11:53:16 +0200

    server function(only listen)

--- main.py
+++ main.py
@@ -5,18 +5,12 @@ async def new_connection(reader, writer):
     print("new_connection!")
 
 
-class Server:
-    def __init__(self, host: str, port: int):
-        self.server_obj = None
-        self.address = (host, port)
+async def start_server(host: str, port: int):
+    server_obj = await asyncio.start_server(new_connection, host, port)
 
-    async def start(self):
-        self.server_obj = await asyncio.start_server(new_connection, *self.address)
-
-        async with self.server_obj:
-            await self.server_obj.serve_forever()
+    async with server_obj:
+        await server_obj.serve_forever()
 
 
 if __name__ == "__main__":
-    server = Server("localhost", 9000)
-    asyncio.run(server.start())
+    asyncio.run(start_server("localhost", 9000))
